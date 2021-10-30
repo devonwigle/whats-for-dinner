bbbc017bd97b352df63f6fe39ccf5f857b6b7f5f
Devon Wigle <devonwigle@gmail.com>
2021-10-30 11:25:47 -0500

    Add sides, mains, and desserts arrays

--- main.js
+++ main.js
@@ -10,3 +10,52 @@ cookButton.addEventListener('click', chooseRecipe);
 // event handlers and functions
 
 //arrays
+var sides = [
+  'Miso Glazed Carrots'
+  'Coleslaw'
+  'Garden Salad'
+  'Crispy Potatoes'
+  'Sweet Potato Tots'
+  'Coconut Rice'
+  'Caeser Salad'
+  'Shrimp Summer Rolls'
+  'Garlic Butter Mushrooms'
+  'Hush Puppies'
+];
+
+var mains = [
+  'paghetti and Meatballs'
+  'Pineapple Chicken'
+  'Shakshuka'
+  'Thai Yellow Curry'
+  'Bibimbap'
+  'Chicken Parmesan'
+  'Butternut Squash Soup'
+  'BBQ Chicken Burgers'
+  'Ramen'
+  'Empanadas'
+  'Chicken Fried Rice'
+  'Sheet Pan Fajitas'
+  'Margarita Pizza'
+];
+
+var desserts = [
+  'Apple Pie'
+  'Lemon Meringue Pie'
+  'Black Forest Cake'
+  'Banana Bread'
+  'Peach Cobbler'
+  'Cheesecake'
+  'Funfetti Cake'
+  'Baklava'
+  'Flan'
+  'Macarons'
+  'Macaroons'
+  'Chocolate Cupcakes'
+  'Pavlova'
+  'Pumpkin Pie'
+  'Key Lime Pie'
+  'Tart Tatin'
+  'Croissants'
+  'Eclairs'
+];
