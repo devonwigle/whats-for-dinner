916aa52114fdc6c162f1b18e60e31a9f97a1112f
Devon Wigle <devonwigle@gmail.com>
2021-10-30 11:23:23 -0500

    Add variables

---
+++ main.js
@@ -0,0 +1,8 @@
+// variables
+var cookButton = document.querySelector('.cook-button');
+
+var sideSelection = document.querySelector('#side');
+var mainDishSelection = document.querySelector('#mainDish');
+var dessertSelection = document.querySelector('#entireMeal');
+
+// eventlisteners
